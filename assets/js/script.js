ece9c6d4806c973333b83c3a36453d8ee344b9dd
DeveloperMen Teams <info@developermen.ir>
2022-10-25 21:18:53 +0330

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+window.onload = function () {
+    const loader = document.getElementById("loader");
+    setTimeout(() => {
+        loader.setAttribute('style','animation: fade 2s; animation-iteration-count: 1;');
+        setTimeout(() => {
+            loader.setAttribute('style','display: none;');
+        }, 2000);
+    }, 2000);
+}
